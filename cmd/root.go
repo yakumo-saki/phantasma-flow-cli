374bc7574b8ebfe6f7ea0499869b1eba1efa00f6
Yakumo Saki <yakumo@ziomatrix.org>
2022-03-04 14:41:15 +0900

    create get jobs entry point

--- cmd/root.go
+++ cmd/root.go
@@ -26,6 +26,8 @@ to quickly create a Cobra application.`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
+var RootCmd = rootCmd
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
